098996a54267ecdcf9f06419cfb7390c2841e702
Krushil Naik <krushilnaik96@gmail.com>
2022-11-26 13:37:18 -0500

    Bug fixes

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
+# Create your models here.
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """Post model"""
+
+    created = models.DateTimeField()
+    title = models.CharField(max_length=64)
+    body = models.CharField(max_length=1200)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
--- pytter_django/urls.py
+++ pytter_django/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path("", views.index, name="Home page"),
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls'))
 ]
